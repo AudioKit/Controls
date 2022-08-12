01902c5e56fd3638770b318893532f29a6f3eb44
Aurelius Prochazka <aurephd@gmail.com>
2022-08-11 21:31:01 -0700

    Add platforms to package

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "DraggableControl",
+    platforms: [.macOS(.v12), .iOS(.v15)],
     products: [.library(name: "DraggableControl",
                         targets: ["DraggableControl"])],
     targets: [
