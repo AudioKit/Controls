1f35d8d7f4be3cc0de0dbbdd03fd7391796d9a90
Aurelius Prochazka <aurephd@gmail.com>
2022-08-12 15:25:00 -0700

    Working demo of rectilinear and polar

--- Demo/Shared/ContentView.swift
+++ Demo/Shared/ContentView.swift
@@ -6,22 +6,21 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Rectangle().foregroundColor(.white)
-            VStack {
-                Spacer()
-                Rectangle()
-                    .foregroundColor(.black)
-                    .draggable(layout: .rectilinear) { x, y in
-                        print("x: \(x), y: \(y)")
+
+            HStack(spacing: 100) {
+                VStack(spacing: 200) {
+                    Draggable(layout: .rectilinear) { x, y in
+                        Rectangle().foregroundColor(.red)
+                            .offset(x: x * 100, y: -y * 100)
                     }
-                    .frame(width: 600, height: 200)
-                Spacer()
-                Circle()
-                    .foregroundColor(.black)
-                    .draggable(layout: .polar(anchor: .bottomLeading)) { r, theta in
-                        print("r \(r), theta \(theta)")
+                    .frame(width: 100, height: 100)
+                    Draggable(layout: .polar) { r, theta in
+                        Rectangle().foregroundColor(.green)
+                            .rotationEffect(Angle(radians: theta * 2.0 * .pi))
                     }
-                    .frame(width: 200, height: 200)
-                Spacer()
+                    .frame(width: 100, height: 100)
+
+                }
             }
         }
     }
