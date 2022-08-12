acede0e376fddafa6c03408f8830ba8a9f6a9d32
Aurelius Prochazka <aurephd@gmail.com>
2022-08-11 21:29:56 -0700

    Added anchor to layout

--- Sources/DraggableControl/ControlLayout.swift
+++ Sources/DraggableControl/ControlLayout.swift
@@ -1,18 +1,24 @@
 import Foundation
+import SwiftUI
 
+import SwiftUI
+
+/// Layouts define how the touch point's location affect the control values
 public enum ControlLayout {
     /// Most sliders, both horizontal and vertical, where you expect
     /// your touch point to be the represent control
     case rectilinear
 
     /// Knobs, or small control areas
-    case relativeRectilinear(xSensitivity: Double, ySensitivity: Double)
+    case relativeRectilinear(xSensitivity: Double = 1.0,
+                             ySensitivity: Double = 1.0)
 
     /// Larger knobs with a more skeumorphic drag and twist motif.
     /// For non relative the values change immediately to match the touch.
-    case polar
+    case polar(anchor: Alignment = .center)
 
     /// This version gives the user more control in the radial direction
     /// and doesn't
-    case relativePolar(radialSensitivity: Double)
+    case relativePolar(anchor: Alignment = .center,
+                       radialSensitivity: Double = 1.0)
 }
