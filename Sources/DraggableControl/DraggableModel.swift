5b85fbc88a90e739162020f775e657677aff2ad7
Aurelius Prochazka <aurephd@gmail.com>
2022-08-12 21:56:45 -0700

    cleanup

--- Sources/DraggableControl/DraggableModel.swift
+++ Sources/DraggableControl/DraggableModel.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
 struct PolarCoordinate {
-    var r: Double
-    var theta: Angle
+    var radius: Double
+    var angle: Angle
 }
 
 class DraggableModel: ObservableObject {
@@ -17,7 +17,7 @@ class DraggableModel: ObservableObject {
             guard touchLocation != oldValue,
                   touchLocation != .zero,
                   oldValue != .zero else { return }
-            
+
             switch layout {
             case .rectilinear:
                 value1 = max(0.0, min(1.0, touchLocation.x / rect.size.width))
@@ -32,15 +32,15 @@ class DraggableModel: ObservableObject {
 
             case .polar:
                 let polar = polarCoordinate(point: touchLocation)
-                value1 = polar.r
-                value2 = max(0.0, min(1.0, polar.theta.radians / (2.0 * .pi)))
+                value1 = polar.radius
+                value2 = max(0.0, min(1.0, polar.angle.radians / (2.0 * .pi)))
 
             case .relativePolar(radialSensitivity: let radialSensitivity):
                 let oldPolar = polarCoordinate(point: oldValue)
                 let newPolar = polarCoordinate(point: touchLocation)
 
-                let temp1 = value1 + (newPolar.r - oldPolar.r) * radialSensitivity
-                let temp2 = value2 + (newPolar.theta.radians - oldPolar.theta.radians) / (2.0 * .pi)
+                let temp1 = value1 + (newPolar.radius - oldPolar.radius) * radialSensitivity
+                let temp2 = value2 + (newPolar.angle.radians - oldPolar.angle.radians) / (2.0 * .pi)
 
                 value1 = max(0, min(1, temp1))
                 value2 = max(0, min(1, temp2))
@@ -52,11 +52,11 @@ class DraggableModel: ObservableObject {
         let deltaX = (point.x - rect.midX) / (rect.width / 2.0)
         let deltaY = (point.y - rect.midY) / (rect.height / 2.0)
 
-        let r = max(0.0, min(1.0, sqrt(pow(deltaX, 2) + pow(deltaY, 2))))
+        let radius = max(0.0, min(1.0, sqrt(pow(deltaX, 2) + pow(deltaY, 2))))
 
         var theta = atan(deltaY / deltaX) + 0.5 * .pi
         if deltaX > 0 { theta += .pi }
 
-        return PolarCoordinate(r: r, theta: Angle(radians: theta))
+        return PolarCoordinate(radius: radius, angle: Angle(radians: theta))
     }
 }
