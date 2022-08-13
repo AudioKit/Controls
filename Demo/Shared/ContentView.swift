e69e5bad3d37e147b81af2e0c04c67f44140c378
Aurelius Prochazka <aurephd@gmail.com>
2022-08-13 12:57:05 -0700

    Added PitchModWheel in place of Fader example

--- Demo/Shared/ContentView.swift
+++ Demo/Shared/ContentView.swift
@@ -5,7 +5,10 @@ struct ContentView: View {
     var body: some View {
         VStack(spacing: 10) {
             HStack(spacing: 100) {
-                Fader().frame(width: 100)
+                HStack {
+                    PitchModWheel(type: .pitch).frame(width: 100)
+                    PitchModWheel(type: .mod).frame(width: 100)
+                }
                 VStack(spacing: 100) {
                     XYPad().frame(width: 200, height: 200)
                     Joystick().frame(width: 200, height: 200)
---
+++ Demo/Shared/Draggable Examples/PitchModWheel.swift
@@ -0,0 +1,44 @@
+import SwiftUI
+import DraggableControl
+
+class PitchWheelModel: ObservableObject {
+    @Published var location = 0.0
+}
+
+enum WheelType {
+    case pitch
+    case mod
+}
+
+struct PitchModWheel: View {
+
+    var type: WheelType
+
+    @StateObject var model = PitchWheelModel()
+
+    var body: some View {
+        GeometryReader { geo in
+            Draggable(layout: .rectilinear,
+                      value1: .constant(0),
+                      value2: $model.location,
+                      onEnded: { if type == .pitch { model.location = 0.5} })
+            {
+                ZStack(alignment: .bottom) {
+                    RoundedRectangle(cornerRadius: 10).foregroundColor(.gray)
+                    RoundedRectangle(cornerRadius: 10).foregroundColor(.red)
+                        .frame(height: geo.size.height / 20)
+                        .offset(y: -model.location * geo.size.height)
+                }.animation(.spring(response: 0.2), value: model.location)
+            }
+        }.onAppear {
+            if type == .pitch { model.location = 0.5 }
+        }
+    }
+}
+
+
+struct Fader_Previews: PreviewProvider {
+    static var previews: some View {
+        PitchModWheel(type: .pitch)
+    }
+}
